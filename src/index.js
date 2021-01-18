7d0d616f19004bbc744f2482980ecf41959453e8
Alina Goncharova <goncarovaalina206@gmail.com>
2021-01-18 14:15:25 +0200

    fetch + markup

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
-
 import './css/common.css';
-import fetchImages from './js/fetch-images';
+import galleryItem from './templates/gallery-item.hbs'; 
+import refs from './js/refs.js';
+
+refs.form.addEventListener('submit', event => {
+    event.preventDefault();
+
+    const inputValue = event.currentTarget.elements.query.value;
+    console.log(event.currentTarget.elements);
+    const apiKey = '12041816-7b3c6fcda05d87e4a56d1e906';
+
+    const url = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${inputValue}&per_page=12&key=${apiKey}`;
+
+fetch(url)
+.then(response => response.json())
+.then(({hits}) => {
+    // console.log(hits);
+    const markup = galleryItem(hits);
+    // console.log(markup)
+    refs.gallery.insertAdjacentHTML('beforeend', markup);
+    })
+} );
 
 
-
-
-// fetch('https://pixabay.com/api/?key=12041816-7b3c6fcda05d87e4a56d1e906&q=yellow+flowers&image_type=photo')
-// .then(res => res.json())
-// .then(({hits}) => console.log(hits));
+    
